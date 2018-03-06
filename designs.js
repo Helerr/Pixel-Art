af939fea2ee30aef8c1737ea1f8a45851199f157
Helerr <alexandruneculai04@gmail.com>
2018-03-06 15:00:48 +0200

    Pixel Art Maker complete

--- designs.js
+++ designs.js
@@ -7,10 +7,34 @@ console.log(color);
 let height;
 let width;
 
+
+const form = document.getElementById('sizePicker');
+
 // When size is submitted by the user, call makeGrid()
-
+form.addEventListener('submit', function(e){
+	e.preventDefault();
+	height = document.getElementById('inputHeight').value;
+	width = document.getElementById('inputWeight').value;
+	console.log(height);	
+	console.log(width);
+	makeGrid();
+});
 function makeGrid() {
-
-// Your code goes here!
+	const canvas = document.getElementById("pixelCanvas");
+	while(canvas.firstChild){
+		canvas.removeChild(canvas.firstChild);
+	}
+	canvas.style.backgroundColor= "white";
+	const col="<td></td>";
+	for(let i=1; i<= height; i++){
+		let row = document.getElementById("pixelCanvas").appendChild(document.createElement("tr"));
+		for(let j=1;j<= width; j++){
+			row.appendChild(document.createElement("td"));
+		}
+	}
 
 }
+
+document.getElementById("pixelCanvas").addEventListener("click", function(e){
+	e.target.style.backgroundColor = color;
+}, false);
