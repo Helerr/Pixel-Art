23fb6f764d8203355fa7e5ade777ea9cc4cc83f5
Helerr <alexandruneculai04@gmail.com>
2018-03-06 13:30:39 +0200

    Color event listener

--- designs.js
+++ designs.js
@@ -1,5 +1,11 @@
 // Select color input
+const colorpicker = document.getElementById('colorPicker');
+let color = colorpicker.value;
+colorpicker.addEventListener('change', function() {color = this.value;}, false);
+console.log(color);
 // Select size input
+let height;
+let width;
 
 // When size is submitted by the user, call makeGrid()
 
